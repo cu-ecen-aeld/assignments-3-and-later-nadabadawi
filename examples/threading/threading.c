a78e243ef7bb633644ae8f97f1872a8631602a2b
nada <nada.badawi@fab-minds.com>
2025-11-07 19:37:44 +0200

    Assignment 4 Part 1 Submission

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -11,23 +12,41 @@
 void* threadfunc(void* thread_param)
 {
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* targs = (struct thread_data *) thread_param;
+
+    usleep(targs->wait_to_obtain_ms * 1000); // 1 sec
+
+    pthread_mutex_lock(targs->mutex);
+
+    usleep(targs->wait_to_release_ms * 1000);
+
+    pthread_mutex_unlock(targs->mutex);
+
+    targs->thread_complete_success = true;
+
     return thread_param;
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
-    /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
-     * using threadfunc() as entry point.
-     *
-     * return true if successful.
-     *
-     * See implementation details in threading.h file comment block
-     */
-    return false;
+    struct thread_data* targs = malloc(sizeof(struct thread_data));
+    if (targs == NULL) {
+        ERROR_LOG("Failed to allocate memory for thread_data");
+        return false;
+    }
+    targs->mutex = mutex;
+    targs->wait_to_obtain_ms = wait_to_obtain_ms;
+    targs->wait_to_release_ms = wait_to_release_ms;
+    targs->thread_complete_success = false;
+
+    int rc = pthread_create(thread, NULL, threadfunc, targs);
+
+    if (rc != 0) {
+        ERROR_LOG("Failed to create the thread!");
+        free(targs);
+        return false;
+    }
+    return true;
 }
 
